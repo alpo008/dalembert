6bd54a98c3d5e0d1c0a39820973f979458bce679
Alexey POZHIDAEV <pozhydaev_alexey@mail.ru>
2023-11-20 19:48:31 +0300

    Text field with copy icon

--- resources/js/components/AppAirmaxClient.vue
+++ resources/js/components/AppAirmaxClient.vue
@@ -1,10 +1,21 @@
 <template>
   <h1> Клиент {{ place }}</h1>
+  <v-text-field 
+    label="Name"
+    ref="name"
+    v-if="!!clientData.name|editable"
+    v-model="clientData.name"
+    :readonly="!editable"
+    append-icon="mdi-content-copy"
+    @click:append="copyText('name')"
+  >
+  </v-text-field>
 </template>
 <script>
 export default {
   data: function () {
     return {
+      editable: true,
       place: '',
       clientData: {}
     }
@@ -16,6 +27,14 @@ export default {
     this.place = this.$route.params.place;
     this.$store.commit('setCurrentClient', this.place);
     this.clientData = this.$store.getters.currentAirmaxClient;
+  },
+  methods:{
+    copyText(txt){
+      const element = this.$refs[txt];
+      element.select();
+      element.setSelectionRange(0, 99999);
+      document.execCommand('copy');
+    }
   }
-}
+}Васильев Андрей Евгньевич
 </script>
