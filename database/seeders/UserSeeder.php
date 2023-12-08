726dab5c1035311c58918a3a79db21f9411a4ea7
Alexey POZHIDAEV <pozhydaev_alexey@mail.ru>
2023-12-08 17:53:05 +0300

    added User seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'User',
+            'email' => 'test@gmail.com',
+            'password' => Hash::make('VERY_SECRET_PASSWORD'),
+            'role' => User::ROLE_USER
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'admin@hotmail.com',
+            'password' => Hash::make('VERY_SECRET_PASSWORD'),
+            'role' => User::ROLE_ADMIN
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Super',
+            'email' => 'superadmin@hotmail.com',
+            'password' => Hash::make('VERY_SECRET_PASSWORD'),
+            'role' => User::ROLE_SUPERADMIN
+        ]);
+    }
+}
