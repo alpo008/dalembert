4448e86d41bafd82b6a74ca74c26cf3d299ede87
Alexey POZHIDAEV <pozhydaev_alexey@mail.ru>
2024-02-21 17:19:11 +0300

    Created Customer model and related table

--- resources/js/store/works.js
+++ resources/js/store/works.js
@@ -13,6 +13,7 @@ export default {
 		setCurrentWork(state, payload) {
 			if(typeof(payload.current) === 'object') {
 				state.current = payload.current;
+				state.all = state.all.filter(el => el.id !== payload.current.id);
 				state.all.push(state.current);
 			}		
 		},
