6af2203b7e397b90f128c1b8cf43e0487a70ebcd
Alexey POZHIDAEV <pozhydaev_alexey@mail.ru>
2026-01-17 10:38:06 +0300

    Corrected language detection for some browsers

--- resources/js/components/widgets/WidgetGweather.vue
+++ resources/js/components/widgets/WidgetGweather.vue
@@ -256,13 +256,12 @@ export default {
     };
   },
   async mounted() {
-    this.language = window.navigator.language;
-
-      try {
-        const response = await axios('https://electromore.ru/api/meteo');
-      } catch (error) {
-        console.error(this._t('Error fetching weather data:'), error);
-      }
+    this.setLanguage();
+    try {
+      const response = await axios('https://electromore.ru/api/meteo');
+    } catch (error) {
+      console.error(this._t('Error fetching weather data:'), error);
+    }
   },
   beforeDestroy() {
     clearInterval(this.timer);
@@ -294,6 +293,25 @@ export default {
         } catch (error) {
           console.error(this._t('Error fetching history data:'), error);
         }
+      }
+    },
+    setLanguage() {
+      this.language = window.navigator.language;
+      if (typeof document === 'object') {
+        let tagHtml = document.getElementsByTagName('html');
+        if(typeof tagHtml === 'object' && typeof tagHtml[0] !== 'undefined') {
+          if(typeof tagHtml[0] === 'object') {
+            if(tagHtml[0].getAttribute('lang') !== null) {
+              this.language = tagHtml[0].getAttribute('lang');
+            }
+          }
+        }
+      } 
+      if(this.language === 'ru') {
+        this.language = 'ru-RU'; 
+      }
+      if(this.language === 'en') {
+        this.language = 'en-US'; 
       }
     },
     _t(txt) {
