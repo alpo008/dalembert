f5cd9cf6cef0529e52a0aa3c816b699851983cfc
Alexey POZHIDAEV <pozhydaev_alexey@mail.ru>
2026-02-02 15:09:34 +0300

    Billboard template on globus page

--- resources/js/components/AppGlobus.vue
+++ resources/js/components/AppGlobus.vue
@@ -1,6 +1,51 @@
 <template>
+  <widget-gweather/>
+  <v-card class="mx-auto" 
+  style="
+    position: fixed;
+    top: 20px;
+    background: transparent;
+    z-index: -1;
+    justify-self: flex-end;
+  ">
+    <v-container fluid>
+      <v-row dense>
+        <v-col
+          v-for="card in cards"
+          :key="card.title"
+          :cols="card.flex"
+        >
+          <v-card theme="dark" style="background-color:rgba(33,33,33, 0.4);">
+            <v-img
+              :src="card.src"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title v-text="card.title"></v-card-title>
+            </v-img>
 
-<widget-gweather/>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+
 </template>
 <script>
   const isEmpty = obj => [Object, Array].includes((obj || {}).constructor) && !Object.entries((obj || {})).length;
@@ -11,6 +56,11 @@
     },
     data: function () {
       return {
+        cards: [
+          { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
+          { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+          { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+        ]
       }
     },
     async created() {
