cfbc9f0b71862a0b76c323ab058772c46c86f646
Alexey POZHIDAEV <pozhydaev_alexey@mail.ru>
2026-02-06 10:05:47 +0300

    Changed sticker design

--- resources/js/components/AppGlobus.vue
+++ resources/js/components/AppGlobus.vue
@@ -3,17 +3,18 @@
   <v-card class="mx-auto" 
   style="
     position: fixed;
-    top: 20px;
+    top: 83px;
     background: transparent;
     z-index: -1;
     justify-self: flex-end;
+    align-self: anchor-center;
   ">
     <v-container fluid>
       <v-row dense>
         <v-col
           v-for="card in cards"
           :key="card.title"
-          :cols="card.flex"
+          max-width="360"
         >
           <v-card theme="dark" style="background-color:rgba(33,33,33, 0.4);">
             <v-img
@@ -23,6 +24,7 @@
               height="200px"
             >
               <v-card-title v-text="card.title"></v-card-title>
+              <v-card-text v-text="card.text"></v-card-text>
             </v-img>
 
             <v-card-actions>
@@ -45,7 +47,6 @@
       </v-row>
     </v-container>
   </v-card>
-
 </template>
 <script>
   const isEmpty = obj => [Object, Array].includes((obj || {}).constructor) && !Object.entries((obj || {})).length;
@@ -57,9 +58,23 @@
     data: function () {
       return {
         cards: [
-          { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
-          { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
-          { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+          { 
+            title: 'Pre-fab homes', 
+            src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg',
+            text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, doloribus, repellat. Suscipit tenetur doloribus corrupti molestiae, aspernatur fugiat. Animi sapiente alias, quam. Quos animi porro enim eveniet cum ab iure.', 
+            flex: 6 
+          },
+          { title: 'Favorite road trips', 
+            src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+            text: 'Suscipit tenetur doloribus corrupti molestiae, aspernatur fugiat. Animi sapiente alias, quam. Quos animi porro enim eveniet cum ab iure.', 
+            flex: 6 
+          },
+          { 
+            title: 'Best airlines', 
+            src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
+            text: 'Animi sapiente alias, quam. Quos animi porro enim eveniet cum ab iure.', 
+            flex: 6 
+          },
         ]
       }
     },
