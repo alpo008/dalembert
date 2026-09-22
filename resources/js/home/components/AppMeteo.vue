402c9969ecdbbc55b6ba90c73b58dc599a236120
Alexey POZHIDAEV <pozhydaev_alexey@mail.ru>
2026-09-22 13:39:23 +0300

    Frontend: meteostation monitor

--- resources/js/home/components/AppMeteo.vue
+++ resources/js/home/components/AppMeteo.vue
@@ -1,5 +1,225 @@
 <template>
-<p>{{ updated_at }}</p>
+
+        {{ $t('Updated at') }} {{ updated_at }}
+        <div class="wrapper_meteo" v-if="!!$store.getters.currentWeather">
+          <div class="params_block_wrapper">
+            <div class="params_block">
+              <div class="temp-box">
+                <div class="link-icon-left chart-link" 
+                  @click="showChart('temperature')" 
+                  :title="$t('Show chart')"
+                >
+                </div>
+                <div class="text-small text-white-blue text-bolder">
+                  {{ $t('Temperature') }}
+                </div>
+                <div class="wx_parameter">
+                  {{ $store.getters.temperatureOut }} 
+                  <span class="text-unit">
+                    {{ $t($store.getters.temperatureUnit) }}
+                  </span>
+                </div>
+                <div class="text-small">
+                  {{ $t('Feels like') }}
+                  <span class="text-green">
+                    {{ $store.getters.feelsLike }} {{ $t($store.getters.feelsLikeUnit) }}
+                  </span>
+                </div>
+              </div>
+              <div class="temp-box">
+                <div class="link-icon-right chart-link" 
+                  @click="showChart('humidity')" 
+                  :title="$t('Show chart')"
+                >
+                </div>
+                <div class="text-small text-white-blue text-bolder">
+                  {{ $t('Humidity') }}
+                </div>
+                <div class="wx_parameter">
+                  {{ $store.getters.humidity }} 
+                  <span class="text-unit">
+                    {{ $t($store.getters.humidityUnit) }}
+                  </span>
+                </div>
+                <div class="text-small">
+                  {{ $t('Dew point') }}
+                  <span class="text-green">
+                    {{ $store.getters.dewPoint }} {{ $t($store.getters.dewPointUnit) }}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+           <div class="params_block_wrapper">
+                <div class="link-icon-left chart-link pt-l-4" 
+                  @click="showChart('pressure')" 
+                  :title="$t('Show chart')"
+                >
+                </div>
+            <div class="text-small text-white-blue text-bolder">
+              {{ $t('Pressure') }} 
+            </div>
+            <div class="params_block">
+              <div class="temp-box">
+                <div class="text-small">
+                  {{ $t('Absolute') }}
+                </div>
+                <div class="wx_parameter">
+                  {{ $store.getters.pressureAbs }}
+                  <span class="text-unit">
+                    {{ $t($store.getters.pressureUnit) }}
+                  </span>
+                </div>
+              </div>
+              <div class="temp-box">
+                <div class="text-small">
+                  {{ $t('Relative') }}
+                </div>
+                <div class="wx_parameter">
+                  {{ $store.getters.pressureRel }}
+                  <span class="text-unit">
+                    {{ $t($store.getters.pressureUnit) }}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="params_block_wrapper">
+                <div class="link-icon-left chart-link pt-l-4" 
+                  @click="showChart('solar')" 
+                  :title="$t('Show chart')"
+                >
+                </div>
+            <div class="text-small text-white-blue text-bolder">
+              {{ $t('Solar and UVI') }}
+            </div>
+            <div class="params_block">
+              <div class="temp-box">
+                <div class="text-small">
+                  {{ $t('Illumination') }}
+                </div>
+                <div class="wx_parameter">
+                  {{ $store.getters.solarRounded.value }}
+                  <span class="text-unit">
+                    {{ $t($store.getters.solarRounded.unit) }}
+                  </span>
+                </div>
+              </div>
+              <div class="temp-box">
+                <div class="text-small">
+                  {{ $t('UVI') }}
+                </div>
+                <div class="wx_parameter">
+                  {{ $store.getters.uvi }}
+                  <span class="text-unit">
+                    {{ $t($store.getters.uviUnit) }}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="params_block_wrapper">
+              <div class="link-icon-left chart-link pt-l-4" 
+                @click="showChart('wind')" 
+                :title="$t('Show chart')"
+              >
+              </div>
+            <div class="text-small text-white-blue text-bolder">
+              {{ $t('Wind') }}
+            </div>
+            <div class="params_block">
+              <div class="temp-box">
+                <div class="text-small">
+                  {{ $t('Speed') }}
+                </div>
+                <div class="wx_parameter">
+                  {{ $store.getters.windSpeed }}
+                  <span class="text-unit">
+                    {{ $t($store.getters.windSpeedUnit) }}
+                  </span>
+                </div>
+              </div>
+              <div class="temp-box height130" v-if="!!$store.getters.windArrowStyle">
+                <div class="wind-arrow" :style="$store.getters.windArrowStyle"></div>
+                <div class="wx_parameter" style="position:relative;top:-100px;">
+                  {{ $store.getters.windDirection }}
+                  <span class="text-unit">
+                    {{ $t($store.getters.windDirectionUnit) }}
+                  </span>
+                  <p class="wind-rumb">{{ $store.getters.windRumb }}</p>
+                </div>
+              </div>
+              <div class="temp-box">
+                <div class="text-small">
+                  {{ $t('Gust') }}
+                </div>
+                <div class="wx_parameter">
+                  {{ $store.getters.windGust }}
+                  <span class="text-unit">
+                    {{ $t($store.getters.windSpeedUnit) }}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+         <div class="params_block_wrapper">
+            <div class="link-icon-left chart-link pt-l-4" 
+              @click="showChart('rainfall')" 
+              :title="$t('Show chart')"
+            >
+            </div>
+            <div class="text-small text-white-blue text-bolder">
+              {{ $t('Rain') }}
+            </div>
+            <div class="params_block">
+              <div class="temp-box">
+                <div class="text-small">
+                  {{ $t('Per hour') }}
+                </div>
+                <div class="wx_parameter">
+                  {{ $store.getters.rainHour }} 
+                  <span class="text-unit">
+                    {{ $t($store.getters.rainUnit) }}
+                  </span>
+                </div>
+                <div class="text-small">
+                  {{ $t('Per day') }}
+                </div>
+                <div class="wx_parameter">
+                  {{ $store.getters.rainDay }} 
+                  <span class="text-unit">
+                    {{ $t($store.getters.rainUnit) }}
+                  </span>
+                </div>
+              </div>
+              <div class="temp-box align-center">
+                <div class="text-small space-between" style="height:2em;">
+                  {{ $t('Weekly') }} <span class="text-green">
+                    {{ $store.getters.rainWeek }} {{ $t($store.getters.rainUnit) }}
+                  </span>
+                </div>
+                <div class="text-small space-between" style="height:2em;">
+                  {{ $t('Monthly') }} <span class="text-green">
+                    {{ $store.getters.rainMonth }} {{ $t($store.getters.rainUnit) }}
+                  </span>
+                </div>
+                <div class="text-small space-between" style="height:2em;">
+                  {{ $t('Yearly') }} <span class="text-green">
+                    {{ $store.getters.rainYear }} {{ $t($store.getters.rainUnit) }}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+ <!--        <div class="wrapper" v-if="mode.showChart">
+          <div class="params_block">
+            <div class="close-icon-right" @click="showChart(null)" :title="$t('Close')">
+                &#65794;
+            </div>
+            <LineChart :history="dataset" style="" />
+          </div>
+      </div>--> 
+              </div>
 </template>
 <script>
   const WEATHER_UPDATES_INTERVAL = 150000;  //TODO 5 minutes
@@ -34,6 +254,8 @@
             mutation: 'setWeatherData'
           });
         }
+      },
+      showChart(param) {
       }
     },
     computed: {
@@ -48,7 +270,231 @@
     }
   }
 </script>
+
 <style scoped>
+/*  html, body {
+      margin: 0;
+      padding: 0;
+  }
+
+  body {
+      font-family: 'Roboto', sans-serif;
+      background-color: #f8fafa;
+      color: #333;
+  }
+
+  .container {
+      max-width: 1200px;
+      margin: 0 auto;
+      padding: 2rem;
+  }
+
+  .header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 2rem;
+  }
+
+  .header h1 {
+      font-family: 'Orbitron', sans-serif;
+      font-size: 2.5rem;
+      color: #2b6cb0;
+      margin: 0;
+  }
+*/
+  .weather {
+      text-align: center;
+      margin: 2px;
+      width: 100%;
+      color: #fff;
+      background-color: rgb(43, 46, 53);
+      border-radius: 1rem;
+      box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.1);
+      transition: width 0.5s linear;
+      padding: 2px 0;
+  }
+
+  .weather h2 {
+      font-size: 2rem;
+      margin-bottom: 0.3rem;
+      margin-block-start: 0.3rem;
+      cursor: pointer;
+      min-height:32px;
+  }
+
+  .wrapper_meteo {
+      display: flex;
+      flex-wrap: wrap;
+      padding: 1px;
+      background-image: radial-gradient(circle, #4A4A4A 0%, #414151 70%, #0D0D0D 100%);
+  }
+
+  .params_block_wrapper {
+      border: 2px solid rgba(141, 172, 45, 0.2);
+      flex-grow: 1;
+      text-align: center;
+   }
+
+  .params_block {
+      display: flex;
+      flex-wrap: wrap;
+      padding: 4px;
+      margin: 0 1px;
+      justify-content: space-between;
+      flex-grow: 1;
+  }
+
+  .temp-box {
+      width: 170px;
+      text-align: center;
+      flex-grow: 1;
+  }
+
+  .wx_parameter {
+      font-size: 35px;
+      font-weight: 700;
+      height: auto;
+      line-height: 52.5px;
+      color: lightsteelblue;
+  }
+
+  .text-small {
+      color: rgb(159, 160, 163);
+  }
+
+  .text-bolder {
+      font-weight: 500;
+  }
+
+  .text-green {
+      color: rgb(141, 172, 45);
+      margin-right: 1px;
+  }
+
+  .text-white-blue {
+      color: lightblue;
+  }
+
+  .wind-rumb {
+      font-size: 1rem;
+      position:relative;
+      top:-28px;
+  }
+
+  .align-center {
+      align-content: center;
+  }
+
+  .justify-start {
+      justify-self: start;
+  }
+
+  .space-between {
+      display: flex;
+      justify-content: space-between;
+  }
+
+  .ml20 {
+      margin-left: 20px;
+  }
+
+  .pt-l-4 {
+      top: 4px;
+      left: 4px;
+  }
+
+  .height130 {
+      height: 130px;
+  }
+
+  .text-unit {
+      font-size: 14px;
+      line-height: 28px;
+      color: rgb(159, 160, 163);
+      vertical-align: text-top;
+  }
+
+  .time {
+      font-size: 1.2rem;
+      color: #4a90e2;
+      margin-bottom: 0.5rem;
+  }
+
+  .temp-max,
+  .temp-min {
+      font-size: 1.2rem;
+      margin-bottom: 0.5rem;
+  }
+
+  .desc {
+      color: #555;
+  }
+
+  .test {
+      min-width: 100%;
+  }
+
+  .wind-arrow {
+      position: relative;
+      background-attachment: scroll;
+      background-clip: border-box;
+      background-color: rgba(0, 0, 0, 0);
+      background-image: url('../assets/wind_arrow.png');
+      background-origin:padding-box;
+      background-position-x: 0%;
+      background-position-y: 0%;
+      background-repeat:no-repeat;
+      background-size:100%;
+      box-sizing:border-box;
+      color:rgb(159, 160, 163);
+      display: inline-flex;
+      font-size:14px;
+      height: 120px;
+      line-height: 21px;
+      margin: 0px;
+      padding: 0px;
+
+      text-size-adjust: 100%;
+      width: 120px;
+      -webkit-font-smoothing: antialiased;
+      -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
+
+  .link-icon-left {
+      width: 20px;
+      height: 20px;
+      position: relative;
+      justify-self: flex-start;
+      cursor: pointer;
+  }
+
+  .link-icon-right {
+      width: 20px;
+      height: 20px;
+      position: relative;
+      justify-self: flex-end;
+      cursor: pointer;
+  }
+
+  .chart-link {
+      background-image: url(../assets/chart.png);
+      background-repeat: no-repeat;
+  }
+
+  .close-icon-right {
+      position: absolute;
+      right: 7px;
+      cursor: pointer;
+      border: 1px solid grey;
+      border-radius: 50%;
+      width: 22px;
+      height: 22px;
+  }
+
+  .link-icon-left:hover, .link-icon-right:hover, .close-icon-right:hover {
+      background-color: darkslategrey;
+  }
     .blur {
       backdrop-filter: blur(10px);
     }
--- resources/js/home/store/meteo.js
+++ resources/js/home/store/meteo.js
@@ -30,6 +30,151 @@ export default {
     },
     weatherUpdatedAt(state) {
       return state.updatedAt;
-    }
+    },
+    temperatureOut(state, getters) {
+      return getters.findOrFail(state, 'current.outdoor.temperature.value') ?? null;
+    },
+    temperatureUnit(state, getters) {
+      return getters.temperatureOut !== null ? 
+        getters.findOrFail(state, 'current.outdoor.temperature.unit') : 
+        '';
+    },
+    feelsLike(state, getters) {
+      return getters.findOrFail(state, 'current.outdoor.feels_like.value') ?? null ;
+      return this.wxData?.outdoor?.feels_like?.value ?? null;
+    },
+    feelsLikeUnit(state, getters) {
+      return getters.feelsLike !== null ? 
+        getters.findOrFail(state, 'current.outdoor.feels_like.unit') : 
+        '';
+    },
+    humidity(state, getters) {
+      return getters.findOrFail(state, 'current.outdoor.humidity.value') ?? null;
+    },
+    humidityUnit(state, getters) {
+      return getters.humidity !== null ? 
+        getters.findOrFail(state, 'current.outdoor.humidity.unit') : 
+        '';
+    },
+    dewPoint(state, getters) {
+      return getters.findOrFail(state, 'current.outdoor.dew_point.value') ?? null;
+    },
+    dewPointUnit(state, getters) {
+      return getters.dewPoint !== null ? 
+        getters.findOrFail(state, 'current.outdoor.dew_point.unit') : 
+        '';
+    },
+    pressureAbs(state, getters) {
+      return getters.findOrFail(state, 'current.pressure.absolute.value') ?? null;
+    },
+    pressureRel(state, getters) {
+      return getters.findOrFail(state, 'current.pressure.relative.value') ?? null;
+    },
+    pressureUnit(state, getters) {
+      return getters.pressureAbs !== null ? 
+        getters.findOrFail(state, 'current.pressure.absolute.unit') : 
+        '';
+    },
+    solar(state, getters) {
+      return getters.findOrFail(state, 'current.solar_and_uvi.solar.value') ?? null;
+    },
+    solarUnit(state, getters) {
+      return getters.solar !== null ? 
+        getters.findOrFail(state, 'current.solar_and_uvi.solar.unit') : 
+        '';
+    },
+    solarRounded(state, getters) {
+      let result = {
+        value: getters.solar,
+        unit: getters.solarUnit
+      };
+      if (getters.solar > 5000) {
+        result.value = (Math.round(getters.solar / 100) / 10),
+        result.unit = 'K' + getters.solarUnit;
+      }
+      return result;
+    },
+    uvi(state, getters) {
+      return getters.findOrFail(state, 'current.solar_and_uvi.uvi.value') ?? null;
+    },
+    uviUnit(state, getters) {
+      return getters.uvi !== null ? 
+        getters.findOrFail(state, 'current.solar_and_uvi.uvi.unit') : 
+        '';
+    },
+    windDirection(state, getters) {
+      return getters.findOrFail(state, 'current.wind.wind_direction.value') ?? null;
+    },
+    windGust(state, getters) {
+      return getters.findOrFail(state, 'current.wind.wind_gust.value') ?? null;
+    },
+    windSpeed(state, getters) {
+      return getters.findOrFail(state, 'current.wind.wind_speed.value') ?? null;
+    },
+    windDirectionUnit(state, getters) {
+      return getters.windDirection !== null ? 
+        getters.findOrFail(state, 'current.wind.wind_direction.unit') : 
+        '';
+    },
+    windSpeedUnit(state, getters) {
+      return getters.windDirection !== null ? 
+        getters.findOrFail(state, 'current.wind.wind_speed.unit') : 
+        '';
+    },
+    windArrowStyle(state, getters) {
+      if (Boolean(getters.windSpeed * 1) || Boolean(getters.windGust * 1)) {
+        return 'transform:rotate(' + getters.windDirection + 'deg)';
+      }
+      return null;
+    },
+    windRumb(state, getters) {
+      if (isNaN(getters.windDirection)) {
+        return "";
+      }
+      let rumb = (getters.windDirection / 1) + 11.25;
+      if (rumb > 360) {
+        rumb = rumb - 360;
+      }
+      let rumbs = {
+        0 :'N', 
+        1 : 'NNE', 
+        2 : 'NE', 
+        3 : 'ENE', 
+        4 : 'E', 
+        5 : 'ESE', 
+        6 : 'SE', 
+        7 : 'SSE', 
+        8 : 'S', 
+        9 : 'SSW', 
+        10 : 'SW', 
+        11 : 'WSW', 
+        12 : 'W', 
+        13 : 'WNW', 
+        14 : 'NW', 
+        15 : 'NNW'
+      };
+      return rumbs[Math.floor(rumb / 22.5)];
+    },
+    rainHour(state, getters) {
+      return getters.findOrFail(state, 'current.rainfall')['1_hour']?.value ?? null;
+    },
+    rainDay(state, getters) {
+      return getters.findOrFail(state, 'current.rainfall.daily.value') ?? null;
+    },
+    rainEvent(state, getters) {
+      return getters.findOrFail(state, 'current.rainfall.event.value') ?? null;
+    },
+    rainWeek(state, getters) {
+      return getters.findOrFail(state, 'current.rainfall.weekly.value') ?? null;
+    },
+    rainMonth(state, getters) {
+      return getters.findOrFail(state, 'current.rainfall.monthly.value') ?? null;
+    },
+    rainYear(state, getters) {
+      return getters.findOrFail(state, 'current.rainfall.yearly.value') ?? null;
+    },
+    rainUnit(state, getters) {
+      return getters.findOrFail(state, 'current.rainfall.daily.unit') ?? '';
+    },
   }
 }
